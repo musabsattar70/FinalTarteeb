cac3b4cb9dca1b9f3cbce53a0804600993c1cb21
musabsattar70 <ms06651@st.habib.edu.pk>
2024-05-08 04:09:09 +0500

    Update ApiRoutes.js

--- Frontend/src/constants/ApiRoutes.js
+++ Frontend/src/constants/ApiRoutes.js
@@ -1,5 +1,5 @@
 // API routes for reports generation solution
 export const API_ROUTES_REPORTS = {
-  BASE_URL: import.meta.env.BASE_URL,
+  BASE_URL: import.meta.env.VITE_API_URL,
   GENERATE_REPORT: "/api/reports/GenerateReport",
 };
